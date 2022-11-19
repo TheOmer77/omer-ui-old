446be671fbe061e99b58e863ea6914e4057972a6
Omer <27678413+TheOmer77@users.noreply.github.com>
2022-11-19 21:35:55 +0200

    Add elevated button variant to button demo

--- demo/pages/ButtonDemo.tsx
+++ demo/pages/ButtonDemo.tsx
@@ -21,7 +21,7 @@ const buttonColors: ButtonColor[] = [
   ],
   buttonIconPositions: ButtonIconPosition[] = ['start', 'end'],
   buttonSizes: ButtonSize[] = ['small', 'medium', 'large'],
-  buttonVariants: ButtonVariant[] = ['text', 'tonal', 'filled'];
+  buttonVariants: ButtonVariant[] = ['text', 'tonal', 'elevated', 'filled'];
 
 const ButtonDemo = () => {
   const [color, setColor] = useState<ButtonColor>('neutral'),
