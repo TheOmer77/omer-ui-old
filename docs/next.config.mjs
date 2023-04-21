391b52e6b96018e33288ec9003a4bcaa0dcc2e3d
Omer <27678413+TheOmer77@users.noreply.github.com>
2023-04-21 20:43:14 +0300

    Move docs webpack config to its own const

--- docs/next.config.mjs
+++ docs/next.config.mjs
@@ -1,40 +1,43 @@
 import nextra from 'nextra';
 
+/**
+ * Enable importing SVGs in react & MDX.
+ * @see [SVGR docs](https://react-svgr.com/docs/next/#usage) (modified to include MDX)
+ * @type {import('next').NextConfig['webpack']}
+ */
+const webpackConfig = config => {
+  // Grab the existing rule that handles SVG imports
+  const fileLoaderRule = config.module.rules.find(rule =>
+    rule.test?.test?.('.svg')
+  );
+
+  config.module.rules.push(
+    // Reapply the existing rule, but only for svg imports ending in ?url
+    {
+      ...fileLoaderRule,
+      test: /\.svg$/i,
+      resourceQuery: /url/, // *.svg?url
+    },
+    // Convert all other *.svg imports to React components
+    {
+      test: /\.svg$/i,
+      issuer: /\.([jt]s|md)x?$/,
+      resourceQuery: { not: /url/ }, // exclude if *.svg?url
+      use: [{ loader: '@svgr/webpack', options: { icon: true } }],
+    }
+  );
+
+  // Modify the file loader rule to ignore *.svg, since we have it handled now.
+  fileLoaderRule.exclude = /\.svg$/i;
+
+  return config;
+};
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
   images: { unoptimized: true },
-  /**
-   * Enable importing SVGs in react & MDX.
-   * @see [SVGR docs](https://react-svgr.com/docs/next/#usage) (modified to include MDX)
-   */
-  webpack(config) {
-    // Grab the existing rule that handles SVG imports
-    const fileLoaderRule = config.module.rules.find(rule =>
-      rule.test?.test?.('.svg')
-    );
-
-    config.module.rules.push(
-      // Reapply the existing rule, but only for svg imports ending in ?url
-      {
-        ...fileLoaderRule,
-        test: /\.svg$/i,
-        resourceQuery: /url/, // *.svg?url
-      },
-      // Convert all other *.svg imports to React components
-      {
-        test: /\.svg$/i,
-        issuer: /\.([jt]s|md)x?$/,
-        resourceQuery: { not: /url/ }, // exclude if *.svg?url
-        use: [{ loader: '@svgr/webpack', options: { icon: true } }],
-      }
-    );
-
-    // Modify the file loader rule to ignore *.svg, since we have it handled now.
-    fileLoaderRule.exclude = /\.svg$/i;
-
-    return config;
-  },
+  webpack: webpackConfig,
 };
 
 const config = nextra({
